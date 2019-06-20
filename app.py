514481ce0b499e3933eb8aab468f204293bab2ca
nsetnik <nsetnik@hotmail.com>
2019-06-19 21:19:26 -0700

    added pages @app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/explore")
+def explore():
+    return render_template('index.html')
+    
+@app.route("/history")
+def history():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
