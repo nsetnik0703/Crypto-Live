05552d153421a87ddc4cfc54ef0a4122f4279838
Naoko <naoon5031@gmail.com>
2019-06-29 11:31:20 -0700

    Added comparison pages and related files.
    Combined app.py.
    Added comparison.html into template
    Added db with sqlite file
    Added png file for comparison page

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask, jsonify, render_template
+from flask import (
+    Flask, 
+    jsonify, 
+    render_template, 
+    request, 
+    redirect)
 import json
 import pandas as pd 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
+#Set Squalchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/CryptoData.sqlite'
+#make a db reference
+db = SQLAlchemy(app)
+
+#create a empty database Model of USD/BTC
+class CryptoData(db.Model):
+    __tablename__ = 'CryptoTable'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # Currency = db.Column(db.String(64))
+    Date = db.Column(db.String(64))
+    Close = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<CryptoData %r>' % (self.Currency)
+    # CryptoData.query.all()
+
+# #Create database tables
+@app.before_first_request
+def setup():
+#     Recreate database each time for demo
+#     db.drop_all()
+    db.create_all()
 
 file = "static/data/historical_data_final.csv"
 historical_df = pd.read_csv(file)
@@ -89,6 +119,29 @@ def historical_data(correctedCurrency):
     }
 
     return jsonify(data)
+#put flask data into element "plot"
+# Query the database and send the jsonified results
+@app.route("/plot_USDBTC")
+def USDBTC_data():
+    # print("hello")
+    #Query for the USDBTC data using pandas
+    results = db.session.query(CryptoData.Date, CryptoData.Close).order_by(CryptoData.Date.desc()).all()
+    # print(results)
+    Date = [result[0] for result in results]
+    Price = [result[1] for result in results]
+    # print(Date, Price)
+
+#(change array to list and set the x and y data)
+
+#Directory change into plot trace
+    trace = {
+        "x": Date,
+        "y": Price,
+        "type": "plot"
+    }
+
+    return jsonify(trace)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
